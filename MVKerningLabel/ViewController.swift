bba092da6f22ddf8cfaa5ffa9fe804cbdd74775a
Andrea Bizzotto <bizz84@gmail.com>
2015-03-13 11:56:24 +0000

    Initial Commit

---
+++ MVKerningLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVKerningLabel
+//
+//  Created by Andrea Bizzotto on 13/03/2015.
+//  Copyright (c) 2015 Muse Visions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
